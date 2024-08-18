983644e9dab4730aa65a7764a9a4b5f1732d1309
Claa <ruloclaa@gmail.com>
2024-08-17 22:31:49 -0300

    first commit

---
+++ programa_1/principal.py
@@ -0,0 +1,16 @@
+import funciones
+
+def main(): 
+    x = int(input("ingrese "))
+    y = int(input("ingrese "))
+    print("la multiplicacion es ", funciones.multiplicar(x,y))
+
+main()
+
+lista = [123, 5432, 142, 232 ,321]
+if 123 in lista: 
+    print("esta")
+
+lista_2 = [505,433,422,44]
+
+
