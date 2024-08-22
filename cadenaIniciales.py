6e1225244eaddc52561ef1ce1131a4807e4f1bbe
Claa <ruloclaa@gmail.com>
2024-08-22 06:25:19 -0300

    cadenas

---
+++ cadenaIniciales.py
@@ -0,0 +1,10 @@
+cadena = "universidad argentina de la empresa"
+cant = cadena.count(" ")
+print(cant)
+aux = cadena.index(" ")
+rebanda = cadena[0:aux]
+print(rebanda)
+rebanda = rebanda.upper()
+print(rebanda)
+inicial = rebanda[0]
+print(inicial)
