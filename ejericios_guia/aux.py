0bcbccbb3c82510ca9520aef48a83c53d8c583f8
Lucio Claa <ruloclaa@gmail.com>
2024-08-28 09:46:19 -0300

    a

--- ejericios_guia/aux.py
+++
@@ -1,8 +0,0 @@
-def verificarFecha(dia,mes,año): 
-    valido = False
-    dias = [31,28,31,30,31,30,31,31,30,31,30,31]
-
-    if dia >= dias[mes]: 
-        valido = True
-        
-    return valido
