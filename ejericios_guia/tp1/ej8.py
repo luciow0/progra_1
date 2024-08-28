ae2e0f0a1be2a51b086361f659bc4a2db46badfb
Lucio Claa <ruloclaa@gmail.com>
2024-08-28 17:57:19 -0300

    archivo

---
+++ ejericios_guia/tp1/ej8.py
@@ -0,0 +1,14 @@
+def diadelasemana(dia,mes,año):
+    if mes < 3:
+        mes = mes + 10
+        año = año - 1
+    else:   
+        mes = mes - 2
+    siglo = año // 100
+    año2 = año % 100
+    diasem = (((26*mes-2)//10)+dia+año2+(año2//4)+(siglo//4)-(2*siglo))%7
+    if diasem < 0:
+        diasem = diasem + 7
+    return diasem
+
+#no entendi nada 
